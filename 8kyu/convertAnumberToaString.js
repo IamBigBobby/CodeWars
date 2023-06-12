dee7086ed6d17876fab58864ebc85795b394d48a
Denis Svetleishii <denissvetleishii@outlook.com>
2023-06-12 19:07:19 +0400

    feat: add solution convert a number to a string

---
+++ 8kyu/convertAnumberToaString.js
@@ -0,0 +1,9 @@
+// We need a function that can transform a number (integer) into a string.
+
+let num = 123;
+
+function numberToString(num) {
+  return num.toString();
+}
+
+console.log(numberToString(num))
