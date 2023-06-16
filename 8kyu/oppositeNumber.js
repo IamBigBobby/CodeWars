df375416741fbc44784975caca846d7e0539312a
Denis Svetleishii <denissvetleishii@outlook.com>
2023-06-16 19:01:56 +0400

    feat: add solution opposote number

---
+++ 8kyu/oppositeNumber.js
@@ -0,0 +1,9 @@
+//  Very simple, given an integer or a floating-point number, find its opposite.
+
+let number = -1
+
+function opposite(number) {
+  return -number;
+}
+
+console.log(opposite(number))
